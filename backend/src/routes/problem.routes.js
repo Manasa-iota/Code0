787187e0bf82d73284dca2d3ccd854ece9806922
ManasaNagaram <nagarammanasa235@gmail.com>
2025-04-26 20:44:02 +0530

    add problem routes

---
+++ backend/src/routes/problem.routes.js
@@ -0,0 +1,24 @@
+import Router from "express"
+
+import { authMiddleware, isAdmin } from "../middlewares/auth.middleware.js"
+import { createProblem, getAllProblems, getProblemById, updateProblem, deleteProblem, getSolvedProblems } from "../controllers/problem.controllers.js";
+
+
+
+
+
+const router = Router();
+
+router.post("/create-problem", authMiddleware, isAdmin, createProblem);
+
+router.get("/get-all-problems", getAllProblems);
+
+router.get("/get-problem/:id", getProblemById);
+
+router.put("/update-problem/:id", authMiddleware, isAdmin, updateProblem);
+
+router.delete("/delete-problem/:id", authMiddleware, isAdmin, deleteProblem);
+
+router.get("/get-solved-problems", authMiddleware, getSolvedProblems)
+
+export default router;

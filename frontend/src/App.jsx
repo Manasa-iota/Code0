e34d9b16e850b8ed2530c4b8e2974df522c577ea
Manasa-iota <nagarammanasa235@gmail.com>
2025-06-18 12:01:00 +0530

    change app.jsx use pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,25 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Loader } from "lucide-react";
 
-import {HomePage, LoginPage, SignUpPage, AddProblem, ProblemPage, ProfilePage} from "./page"
-
-
 import Layout from "./layout/Layout";
 import AdminRoute from "./components/AdminRoute";
+
+import {
+  HomePage,
+  LoginPage,
+  SignUpPage,
+  AddProblemPage,
+  ProblemDetailsPage,
+  ProfilePage
+} from "./pages"; 
+
 import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
-    (async () => {
-      await checkAuth();
-    })();
+    checkAuth();
   }, [checkAuth]);
 
   if (isCheckingAuth && !authUser) {
@@ -33,37 +38,16 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={authUser ? <HomePage /> : <Navigate to="/login" />}
-          />
+          <Route index element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         </Route>
 
-        <Route
-          path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-
-        <Route
-          path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-
-        <Route
-          path="/problem/:id"
-          element={authUser ? <ProblemPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/profile"
-          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-        />
-
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/problem/:id" element={authUser ? <ProblemDetailsPage /> : <Navigate to="/login" />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 
         <Route element={<AdminRoute />}>
-          <Route
-            path="/add-problem"
-            element={authUser ? <AddProblem /> : <Navigate to="/" />}
-          />
+          <Route path="/add-problem" element={authUser ? <AddProblemPage /> : <Navigate to="/" />} />
         </Route>
       </Routes>
     </div>
